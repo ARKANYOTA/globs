97c77e616928b9fbc3b2099578f3dd522df41a8f
Yolwoocle <54135715+Yolwoocle@users.noreply.github.com>
2024-08-16 23:25:18 +0200

    Click areas & expand function

--- scripts/block.gd
+++ scripts/block.gd
@@ -7,27 +7,10 @@ enum Direction {
 }
 
 @export var is_gravity_enabled = true
-
 @export var dimensions: Vector2i = Vector2i(16, 16):
 	set(value):
 		dimensions = value
-		var collision_shape = $CollisionShape
-		var click_area = $ClickArea
-		var click_area_collision_shape = $ClickArea/ClickAreaCollisionShape
-		var shape = collision_shape.shape
-		if shape is not RectangleShape2D:
-			print("$CollisionShape.shape is not a RectangleShape2D")
-			return
-		
-		# Update size
-		shape.size = value
-		click_area_collision_shape.shape = shape
-		
-		# Update position
-		var child_pos: Vector2 = Vector2(dimensions)/2
-		collision_shape.position = child_pos
-		click_area.position = child_pos
-		update_sprite_size(child_pos)
+		_set_dimensions(value)
 
 @export var rotatable: bool = false
 @export_range(-360, 360) var angle: float = 0:
@@ -36,13 +19,9 @@ enum Direction {
 		$CollisionShape.rotation_degrees = value
 		$Sprite.rotation_degrees = value
 
-var is_hovered := false
-
 @onready var label := $Label
 
-func expand(direction: Direction, amount: int):
-	if direction == Direction.RIGHT:
-		dimensions.x += amount
+var is_hovered := false
 
 @export_group("Up Extandable")
 @export var up_extendable: bool = false
@@ -60,7 +39,26 @@ func expand(direction: Direction, amount: int):
 @export var right_extendable: bool = false
 @export var right_extend_range: Vector2i = Vector2i(5, 50)
 
-func update_sprite_size(pos):
+func _set_dimensions(value):
+	var collision_shape = $CollisionShape
+	var click_area = $ClickArea
+	var click_area_collision_shape = $ClickArea/ClickAreaCollisionShape
+	var shape = collision_shape.shape
+	if shape is not RectangleShape2D:
+		print("$CollisionShape.shape is not a RectangleShape2D")
+		return
+	
+	# Update size
+	shape.size = value
+	click_area_collision_shape.shape = shape
+	
+	# Update position
+	var child_pos: Vector2 = Vector2(dimensions)/2
+	collision_shape.position = child_pos
+	click_area.position = child_pos
+	_update_sprite_size(child_pos)
+
+func _update_sprite_size(pos):
 	var sprite: Sprite2D = $Sprite
 	
 	sprite.position = pos
@@ -69,16 +67,27 @@ func update_sprite_size(pos):
 	sprite.scale.x = dimensions.x / sprite_size.x
 	sprite.scale.y = dimensions.y / sprite_size.y
 
+func expand(direction: Direction, amount: int):
+	if direction == Direction.RIGHT and right_extendable:
+		dimensions.x += amount
+	elif direction == Direction.LEFT and left_extendable:
+		dimensions.x += amount
+		position.x -= amount
+	elif direction == Direction.DOWN and down_extendable:
+		dimensions.y += amount
+	elif direction == Direction.UP and up_extendable:
+		dimensions.y += amount
+		position.y -= amount
+
 func _ready():
 	dimensions = dimensions
 
 func _process(delta):
+	if Engine.is_editor_hint():
+		return
+	
 	var area: ClickArea = $ClickArea
 	label.text = str(area.is_hovered)
-	
-	if area.is_hovered:
-		if Input.is_action_just_pressed("left_click"):
-			expand(Direction.RIGHT, 4)
 
 func _physics_process(delta):
 	if is_gravity_enabled and not is_on_floor():
@@ -86,5 +95,5 @@ func _physics_process(delta):
 
 	move_and_slide()
 
-func _on_scale_handle_mouse_entered():
-	pass # Replace with function body.
+func _on_click_area_clicked():
+	expand(Direction.UP, 4)
--- scripts/click_area.gd
+++ scripts/click_area.gd
@@ -1,10 +1,40 @@
+@tool
 extends Area2D
 class_name ClickArea
 
+signal clicked
+
+@export var size: Vector2i:
+	set(value):
+		size = value
+		var shape = $ClickAreaCollisionShape
+		if shape:
+			$ClickAreaCollisionShape.shape.size = Vector2(value)
+
+@export var debug_color: Color = Color(1, 0, 1, 0.5):
+	set(value):
+		debug_color = value
+		var shape = $ClickAreaCollisionShape
+		if shape:
+			$ClickAreaCollisionShape.debug_color = value
+
 var is_hovered := false
 
+func _input(event):
+	if Engine.is_editor_hint():
+		return
+	
+	if is_hovered and event.is_action_pressed("left_click"):
+		clicked.emit()
+
 func _on_mouse_entered():
+	if Engine.is_editor_hint():
+		return
+	
 	is_hovered = true
 
 func _on_mouse_exited():
+	if Engine.is_editor_hint():
+		return
+	
 	is_hovered = false
