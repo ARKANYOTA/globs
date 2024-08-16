9791e8317d42fa02867a318afe380de4a7a1eb88
Yolwoocle <54135715+Yolwoocle@users.noreply.github.com>
2024-08-16 20:07:39 +0200

    ScalableBlock top left anchor

--- scripts/scalable_block.gd
+++ scripts/scalable_block.gd
@@ -1,19 +1,24 @@
 @tool
 extends CharacterBody2D
 
-@export var dimensions: Vector2 = Vector2(16, 16):
+@export var dimensions: Vector2i = Vector2i(16, 16):
 	set(value):
 		dimensions = value
-		var shape = $CollisionShape.shape
+		var collision_shape = $CollisionShape
+		var shape = collision_shape.shape
 		if shape is not RectangleShape2D:
-			print("not a rect")
+			print("$CollisionShape.shape is not a RectangleShape2D")
 			return
 		shape.size = value
 		
-		update_sprite_size()
+		var child_pos: Vector2 = position + Vector2(dimensions)/2
+		collision_shape.position = child_pos
+		update_sprite_size(child_pos)
 
-func update_sprite_size():
+func update_sprite_size(pos):
 	var sprite: Sprite2D = $Sprite
+	
+	sprite.position = pos
 	
 	var sprite_size = sprite.texture.get_size()
 	sprite.scale.x = dimensions.x / sprite_size.x
