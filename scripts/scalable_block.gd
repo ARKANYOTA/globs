5cc41faf8817f1d03ae2ce81b0207020eeb7545b
Yolwoocle <54135715+Yolwoocle@users.noreply.github.com>
2024-08-16 19:57:47 +0200

    ScalableBlock that scales, with sprite

--- scripts/scalable_block.gd
+++ scripts/scalable_block.gd
@@ -1,7 +1,26 @@
+@tool
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var dimensions: Vector2 = Vector2(16, 16):
+	set(value):
+		dimensions = value
+		var shape = $CollisionShape.shape
+		if shape is not RectangleShape2D:
+			print("not a rect")
+			return
+		shape.size = value
+		
+		update_sprite_size()
+
+func update_sprite_size():
+	var sprite: Sprite2D = $Sprite
+	
+	var sprite_size = sprite.texture.get_size()
+	sprite.scale.x = dimensions.x / sprite_size.x
+	sprite.scale.y = dimensions.y / sprite_size.y
+
+func _ready():
+	pass
 
 func _physics_process(delta):
 	move_and_slide()
