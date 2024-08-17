20b4fd6506c5acb667ef6351390135d07c0501c2
ARKANYOTA <arkanyota@icloud.com>
2024-08-17 23:19:53 +0200

    particule de joie

--- scripts/autoload/scene_transition_auto_load.gd
+++ scripts/autoload/scene_transition_auto_load.gd
@@ -2,12 +2,14 @@ extends Node
 
 var scene_transition: PackedScene = preload("res://scenes/scene_transition.tscn")
 var scene_transition_instance: Node
+var youwinlevel: PackedScene = preload("res://you_win_level.tscn")
+var youwinlevel_instance: Node
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#load the scene transition scene
 	scene_transition_instance = scene_transition.instantiate()
 	add_child(scene_transition_instance)
-	pass # Replace with function body.
+	youwinlevel_instance = youwinlevel.instantiate()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,9 +17,16 @@ func _process(delta: float) -> void:
 	pass
 
 func change_scene_with_transition(scene: String) -> void:
+	var root = get_tree().get_current_scene()
+	var player = root.get_node("Player")
+	if not player == null:
+		player.add_child(youwinlevel_instance)
+
 	var animation_player : AnimationPlayer = scene_transition_instance.get_node("AnimationPlayer")
 	animation_player.play("bloc_in")
 	await animation_player.animation_finished
+	if not player == null:
+		player.remove_child(youwinlevel_instance)
 	get_tree().change_scene_to_file(scene)
 	animation_player.play("bloc_in", -1, -0.7, true)
 	pass
