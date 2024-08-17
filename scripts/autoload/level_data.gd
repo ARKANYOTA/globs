6a806e4ad13ee8e212fb7714f79912e3d11356c7
Guigui <113891088+Notgoyome@users.noreply.github.com>
2024-08-17 12:02:19 +0200

    add menu manager as scene and autoload it at the start of the scene

---
+++ scripts/autoload/level_data.gd
@@ -0,0 +1,33 @@
+extends Node
+
+var levels = {
+	1: { "name": "Level 1", "scene": "res://scenes/scene_1.tscn"},
+	2: { "name": "Level 2", "scene": "res://scenes/scene_2.tscn"},
+}
+
+var level = 1
+
+func increment_level() -> void:
+	level += 1
+	if level > len(levels):
+		assert(false, "Level out of bounds")
+	save_level_data()
+
+func save_level_data() -> void:
+	var config = ConfigFile.new()
+	config.set_value("level_section","level", level)
+	config.save("user://level_data.cfg")
+
+func load_level_data() -> void:
+	var config = ConfigFile.new()
+	config.load("user://level_data.cfg")
+	level = config.get_value("level_section","level", level)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
