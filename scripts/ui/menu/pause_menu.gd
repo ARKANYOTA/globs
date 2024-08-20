c3f138c82a3439202d58a881200f561e8772d1a7
Yolwoocle <54135715+Yolwoocle@users.noreply.github.com>
2024-08-20 03:05:26 +0200

    Skip level

--- scripts/ui/menu/pause_menu.gd
+++ scripts/ui/menu/pause_menu.gd
@@ -3,6 +3,7 @@ class_name PauseMenu
 
 @onready var main = get_parent()
 @onready var click_audio = $ClickAudio
+@onready var skip_button = $MarginContainer/Items/Buttons/Skip
 
 func _on_resume_pressed():
 	main.exit_menu()
@@ -10,6 +11,7 @@ func _on_resume_pressed():
 func _on_restart_pressed():
 	main.exit_menu()
 	LevelData.reload_scene()
+	GameManager.on_restart()
 
 func _on_quit_pressed():
 	get_tree().quit()
@@ -18,5 +20,7 @@ func _on_levels_pressed():
 	main.exit_menu()
 	SceneTransitionAutoLoad.change_scene_with_transition("res://scenes/ui/level_select.tscn", false)
 
+
 func _on_skip_pressed():
-	pass # Replace with function body.
+	main.exit_menu()
+	LevelData.win()
