55f019b15cb8f173c201c47a9c3b82c9e15c9cec
Guigui <113891088+Notgoyome@users.noreply.github.com>
2024-08-20 14:13:10 +0200

    ADD you win level, changed sound for you win, better world decoration, removed planet

--- scripts/autoload/level_data.gd
+++ scripts/autoload/level_data.gd
@@ -34,7 +34,7 @@ var levels = [
 	{ "name": "4-5", "music": "snow", "scene": "res://scenes/levels/world_4/level_440.tscn"},
 	{ "name": "4-6", "music": "snow", "scene": "res://scenes/levels/world_4/level_490_guigui.tscn"},
 	{ "name": "4-7", "music": "snow", "scene": "res://scenes/levels/world_4/level_500_guigui.tscn"},
-	{ "name": "You Win", "music": "cheese", "scene": "res://scenes/levels/you_win.tscn"},
+	{ "name": "You Win", "music": "city", "scene": "res://scenes/levels/you_win.tscn"},
 ]
 
 var names = [
