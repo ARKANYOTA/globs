be12b453cf66a40997593082fa1ed6ae2583c715
Guigui <113891088+Notgoyome@users.noreply.github.com>
2024-08-25 03:04:55 +0200

    FIX music, bird make louder sound and are slower, can now click on little dot to change world

---
+++ src/scripts/ui/menu/World/button_index.gd
@@ -0,0 +1,21 @@
+extends Button
+
+var index: int = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_pressed() -> void:
+
+	var world_select = get_node("/root/WorldSelect")
+	if world_select:
+		world_select.world_index = index
+		world_select.change_world(index, false)
+	pass # Replace with function body.
