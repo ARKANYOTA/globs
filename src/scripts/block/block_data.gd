804c1957fed341a9b50af3f1217262833a4c9a64
ARKANYOTA <arkanyota@icloud.com>
2024-08-25 15:56:05 +0200

    undo button is here, faut l'enlever des menus inutils et eviter que ça fasse un go_to_..action quand on deplace un block pas deplacable(retracter un block de 1)

---
+++ src/scripts/block/block_data.gd
@@ -0,0 +1,19 @@
+extends Node
+class_name BlockData
+
+var position
+var up_extend_value
+var down_extend_value
+var right_extend_value
+var left_extend_value
+
+
+func _init(child: Block):
+	position = child.position
+	up_extend_value = child.up_extend_value
+	down_extend_value = child.down_extend_value
+	right_extend_value = child.right_extend_value
+	left_extend_value = child.left_extend_value
+	
+func _to_string():
+	return "["+str(position)+",("+str(up_extend_value)+", "+str(down_extend_value)+","+str(right_extend_value)+","+str(left_extend_value)+")]"
