9adee5430826a91ca4054bbbc0bcd487d57a0862
Yolwoocle <54135715+Yolwoocle@users.noreply.github.com>
2024-09-24 11:12:12 +0200

    Options files & fullscreen shortcut

--- src/scripts/ui/volume_slider.gd
+++ src/scripts/ui/volume_slider.gd
@@ -10,17 +10,22 @@ func _ready() -> void:
 	pass
 
 func _on_value_changed(input_value: float) -> void:
-	var config = ConfigFile.new()
-	config.load("user://volume.cfg")
-
-	var music_value = config.get_value("Sound", "Music", 1)
-	var master_value = config.get_value("Sound", "Master", 1)
-	config.set_value("Sound", "Music", music_value)
-	config.set_value("Sound", "Master", master_value)
-	if audio_bus_name == "Master":
-		config.set_value("Sound", "Master", input_value)
-	else:
-		config.set_value("Sound", "Music", input_value)
-	config.save("user://volume.cfg")
-	
+	GameManager.save_option("volume", audio_bus_name, value)
 	AudioServer.set_bus_volume_db(_bus, linear_to_db(input_value))
+
+
+# func _old_on_value_changed(input_value: float) -> void:
+# 	var config = ConfigFile.new()
+# 	config.load("user://volume.cfg")
+
+# 	var music_value = config.get_value("Sound", "Music", 1)
+# 	var master_value = config.get_value("Sound", "Master", 1)
+# 	config.set_value("Sound", "Music", music_value)
+# 	config.set_value("Sound", "Master", master_value)
+# 	if audio_bus_name == "Master":
+# 		config.set_value("Sound", "Master", input_value)
+# 	else:
+# 		config.set_value("Sound", "Music", input_value)
+# 	config.save("user://volume.cfg")
+	
+# 	AudioServer.set_bus_volume_db(_bus, linear_to_db(input_value))
