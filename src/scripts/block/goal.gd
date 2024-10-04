6e9ac6cf11a4161b1ef27fc4e6e2b3b6b225e902
Yolwoocle <54135715+Yolwoocle@users.noreply.github.com>
2024-10-04 10:36:39 +0200

    New star colors

--- src/scripts/block/goal.gd
+++ src/scripts/block/goal.gd
@@ -2,22 +2,26 @@
 extends Area2D
 class_name Goal
 
-@export var WIN_TIME_DELAY = 1.4
-
-var is_collected = false
-
-@export var do_animation = true
-
-@onready var collision_shape = $CollisionShape
-
-var size_pixels: Vector2
-var overlapping_bodies = Dictionary()
+@export_enum("yellow", "blue", "red", "green") var style: String = "yellow":
+	set(value):
+		style = value 
+		$Sprite.animation = value
+@export var do_collect_animation = true
+@export var win_time_delay = 1.4
 @export var size: Vector2i = Vector2i(1, 1):
 	set(value):
 		size = value
 		size_pixels = Vector2(value) * 16
 
+var is_collected = false
+var size_pixels: Vector2
+var overlapping_bodies = Dictionary()
+
+@onready var collision_shape = $CollisionShape
+
 func _ready():
+	$Sprite.animation = style
+
 	collision_shape.shape = collision_shape.shape.duplicate()
 	collision_shape.shape.size.x = max(size_pixels.x - 2, 0)
 	collision_shape.shape.size.y = max(size_pixels.y - 2, 0)
@@ -39,12 +43,12 @@ func win():
 		return
 	is_collected = true
 
-	if do_animation:
+	if do_collect_animation:
 		$AnimationPlayer.play("collected")
 		$SparkleBurst.emitting = true
 
 		var timer: Timer = $WinDelayTimer
-		timer.set_wait_time(WIN_TIME_DELAY)
+		timer.set_wait_time(win_time_delay)
 		timer.start()
 		await timer.timeout
 	GameManager.win()
