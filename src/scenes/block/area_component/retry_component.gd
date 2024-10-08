2af6a038e98ac50123ac8aaddbe56ebdccb4a105
Guigui <113891088+Notgoyome@users.noreply.github.com>
2024-10-09 00:37:21 +0200

    ADD ACHIEVEMENT

--- src/scenes/block/area_component/retry_component.gd
+++ src/scenes/block/area_component/retry_component.gd
@@ -1,7 +1,9 @@
 extends Node2D
-
+@export var achievement = false
 func reload() -> void:
-    AchievementData.increment_retry_area_count()
+    if achievement:
+        print("ACH_GLOBS_IN_VOID_5_TIMES")
+        AchievementData.increment_retry_area_count()
     LevelData.reload_scene()
 
 func _on_retry_block_on_glob_touched() -> void:
