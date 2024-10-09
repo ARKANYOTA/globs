684cc87c88af15629372951cae4759d8c0eece32
Guigui <113891088+Notgoyome@users.noreply.github.com>
2024-10-10 00:56:50 +0200

    ADDED SECRET WORL ACHIEVEMENT

--- src/scripts/ui/menu/World/scroll_component.gd
+++ src/scripts/ui/menu/World/scroll_component.gd
@@ -3,13 +3,19 @@ extends Node2D
 class_name ScrollComponent
 
 var is_dragging = false
+var mouse_start_click_position = Vector2()
 var mouse_click_position = Vector2()
+var delta_mouse_position = Vector2()
 var mouse_unclick_position = Vector2()
 var delta_max = 32
+var world_screen_size = Vector2(0,0)
 
 @export var world_select : WorldSelect
 
 func _ready():
+	#SCOTCH JE SAIS PAS COMMENT AVOIR LA SIZE DU VIEWPORT
+	# world_screen_size = get_viewport().size
+	world_screen_size = 240
 	pass
 
 func _input(event: InputEvent) -> void:
@@ -21,6 +27,7 @@ func _input(event: InputEvent) -> void:
 			if is_dragging == false:
 				return
 			is_dragging = false
+			delta_mouse_position = Vector2()
 			mouse_unclick_position = event.position
 			if (mouse_click_position.x - mouse_unclick_position.x) > delta_max:
 				world_select.increment_world_index(false)
@@ -32,6 +39,21 @@ func _input(event: InputEvent) -> void:
 	elif event is InputEventMouseMotion:
 		if is_dragging:
 			var delta = event.relative
+			delta_mouse_position += delta
 			for child in get_children():
 				child.position.x += delta.x
-			pass
+			handle_sus_globs_achievements()
+
+func handle_sus_globs_achievements() -> void:
+	var screen_gap = world_screen_size * 0.60
+	var sign = 0
+
+	if world_select.world_index == 0:
+		sign = 1
+	if world_select.world_index == world_select.world.size() - 1:
+		sign = -1
+
+	print(sign * delta_mouse_position.x, " ", screen_gap)
+	if (sign != 0 && sign * delta_mouse_position.x > screen_gap):
+		if GameManager.achievement_manager:
+			GameManager.achievement_manager.grant("ACH_SECRET_WORLD_SELECT")
