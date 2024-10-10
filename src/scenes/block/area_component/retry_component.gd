c62ca8589af8fa6e136b4f6c8770f549a9dc0f7c
ARKANYOTA <arkanyota@icloud.com>
2024-10-10 23:29:50 +0200

    birds achivement

--- src/scenes/block/area_component/retry_component.gd
+++ src/scenes/block/area_component/retry_component.gd
@@ -1,11 +1,11 @@
 extends Node2D
 @export var achievement = false
 func reload() -> void:
-    if achievement:
-        print("ACH_GLOBS_IN_VOID_5_TIMES")
-        AchievementData.increment_retry_area_count()
-    LevelData.reload_scene()
+	if achievement:
+		print("ACH_GLOBS_IN_VOID_5_TIMES")
+		AchievementData.increment_retry_area_count()
+	LevelData.reload_scene()
 
 func _on_retry_block_on_glob_touched() -> void:
-    reload()
-    pass # Replace with function body.
+	reload()
+	pass # Replace with function body.
