2b59b1b2770351d8aee34b47fe97823f4f761353
Guillame Tran <guillaume1.tran@epitech.eu>
2025-12-19 22:50:27 +0900

    add depop globs on border touched

---
+++ src/scenes/block/area_component/area_block_depop_component.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+@export var smoke_scene: PackedScene
+
+func _on_glob_touched(area:Block):
+	var smoke: SmokeParticle = smoke_scene.instantiate()
+	smoke.global_position = area.global_position
+	get_tree().current_scene.add_child(smoke)
+	area.hide()
---
+++ src/scenes/ui/particle/smoke.gd
@@ -0,0 +1,11 @@
+extends CanvasGroup
+class_name SmokeParticle
+
+@onready var cpu_particles_2d: CPUParticles2D = %CpuParticles2D
+
+func _ready() -> void:
+    var tween = create_tween()
+    tween.tween_property(self, "self_modulate:a", 0.0, 1.0)
+    cpu_particles_2d.emitting = true
+    await cpu_particles_2d.finished
+    queue_free()
